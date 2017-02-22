d761f5d0331ed1f5a0b80d0b92ccc4f543f925ea
StevenMHernandez <contact@stevenhdesigns.com>
2017-02-22 17:50:14 -0500

    assigment1: initial commit

---
+++ cmsc257/assign1/cmsc257-s17-assign1-support.h
@@ -0,0 +1,44 @@
+#ifndef CMSC257_A1SUPPORT_INCLUDED
+#define CMSC257_A1SUPPORT_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File          : cmsc257-s17-assign1-support.h
+//  Description   : This is a set of general-purpose utility functions we use
+//                  for the 257 assignment #1.
+//
+//  Author   : ????
+//  Created  : ????
+
+//
+// Functional Prototypes
+
+int float_display_array(????);
+	// This function prints out the array of floating point values
+
+int integer_display_array(????);
+	// This function prints out the array of integer values
+
+int float_evens(????);
+	// Return the number of even numbers in the array (float version)
+
+int integer_evens(????);
+	// Return the number of even numbers in the array (int version)
+
+int countBits(????);
+	// Count the number of '1' bits in the binary representation
+
+int most_values(????);
+	// Print out the values with the most occurences in array
+
+unsigned short reverseBits(????);
+        // Reverse the binary bit string
+
+void binaryString(????);
+        // Print the binary equivalent of the number passed
+
+void integerQuickSort(????);
+        // Sort the integer array using QuickSort algorithm
+        
+
+#endif // CMSC257_A1SUPPORT_INCLUDED
