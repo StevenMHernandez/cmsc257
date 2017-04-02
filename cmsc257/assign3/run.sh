c865775a23073e51ef6d45726efd569717cf2fb8
StevenmHernandez <hernandezsm@vcu.edu>
2017-04-02 14:36:04 -0400

    assign3: make Makefile more explicit

--- cmsc257/assign3/run.sh
+++ cmsc257/assign3/run.sh
@@ -1,16 +1,19 @@
 # build everything
 make clean
-make
+make normalmat
+make transposemat
+make blockedmat
+make threadedmat
 
 echo ""
 echo "running in the background."
 echo "check back later on"
 
-echo 1024 | ./normalmat > reports/normalmat.txt &
-echo 1024 | ./transposemat > reports/transposemat.txt &
-echo 1024 | ./blockedmat 2 > reports/blockedmat_2.txt &
-echo 1024 | ./blockedmat 4 > reports/blockedmat_4.txt &
-echo 1024 | ./blockedmat 8 > reports/blockedmat_8.txt &
-echo 1024 | ./threadedmat 2 > reports/threadedmat_2.txt &
-echo 1024 | ./threadedmat 4 > reports/threadedmat_4.txt &
-echo 1024 | ./threadedmat 8 > reports/threadedmat_8.txt &
+echo 16384 | ./normalmat > reports/normalmat.txt &
+echo 16384 | ./transposemat > reports/transposemat.txt &
+echo 16384 | ./blockedmat 2 > reports/blockedmat_2.txt &
+echo 16384 | ./blockedmat 4 > reports/blockedmat_4.txt &
+echo 16384 | ./blockedmat 8 > reports/blockedmat_8.txt &
+echo 16384 | ./threadedmat 2 > reports/threadedmat_2.txt &
+echo 16384 | ./threadedmat 4 > reports/threadedmat_4.txt &
+echo 16384 | ./threadedmat 8 > reports/threadedmat_8.txt &
