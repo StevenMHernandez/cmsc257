4945e7df71ba0b1a6909e44a0a245b22c0056d3d
StevenmHernandez <hernandezsm@vcu.edu>
2017-04-02 19:37:14 -0400

    assign3: changed to 4096

--- cmsc257/assign3/run.sh
+++ cmsc257/assign3/run.sh
@@ -9,11 +9,11 @@ echo ""
 echo "running in the background."
 echo "check back later on"
 
-echo 16384 | ./normalmat > reports/normalmat.txt &
-echo 16384 | ./transposemat > reports/transposemat.txt &
-echo 16384 | ./blockedmat 2 > reports/blockedmat_2.txt &
-echo 16384 | ./blockedmat 4 > reports/blockedmat_4.txt &
-echo 16384 | ./blockedmat 8 > reports/blockedmat_8.txt &
-echo 16384 | ./threadedmat 2 > reports/threadedmat_2.txt &
-echo 16384 | ./threadedmat 4 > reports/threadedmat_4.txt &
-echo 16384 | ./threadedmat 8 > reports/threadedmat_8.txt &
+echo 4096 | ./normalmat > reports/normalmat.txt &
+echo 4096 | ./transposemat > reports/transposemat.txt &
+echo 4096 | ./blockedmat 2 > reports/blockedmat_2.txt &
+echo 4096 | ./blockedmat 4 > reports/blockedmat_4.txt &
+echo 4096 | ./blockedmat 8 > reports/blockedmat_8.txt &
+echo 4096 | ./threadedmat 2 > reports/threadedmat_2.txt &
+echo 4096 | ./threadedmat 4 > reports/threadedmat_4.txt &
+echo 4096 | ./threadedmat 8 > reports/threadedmat_8.txt &
