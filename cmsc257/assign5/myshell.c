5d8d0ace5c22928eb628c5c569552defc218a91f
StevenMHernandez <contact@stevenhdesigns.com>
2017-04-20 19:27:04 -0400

    fixed exec and parsing, fixed empty line handling

--- cmsc257/assign5/myshell.c
+++ cmsc257/assign5/myshell.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
 
 char input[51];
 
@@ -16,61 +17,72 @@ int main(int argc, char const *argv[]) {
                 }
                 fflush(stdout);
 
+                // clear input
+                input[0] = '\0';
+
                 // wait for input
                 if (fgets(input, 51, stdin) != NULL)
                 {
-                        // remove useless "\n" from fgets
-                        char *newline = strchr( input, '\n' );
-                        if ( newline ) {
-                                *newline = 0;
-                        }
+                        if (input[0] != '\n') {
+                                // remove useless "\n" from fgets
+                                char *newline = strchr( input, '\n' );
+                                if ( newline ) {
+                                        *newline = 0;
+                                }
 
-                        printf("%s|\n", input);
+                                // make a copy, so that we don't tokenize the parameters after the command
+                                char copy[50];
+                                strcpy(copy, input);
 
-                        char *command = strtok(input, " ");
+                                char *parameters;
 
-                        printf("%s|%s|\n", input, command);
+                                char *command = strtok(input, " ");
 
-                        char *parameters = strtok(NULL, input);
+                                parameters = (command) + strlen(command) + 1;
 
-                        printf(":%s::%s\n", command, parameters);
-
-                        if (strncmp(command, "cd", 2) == 0) {
-                                if (parameters == NULL || strlen(parameters) == 0) {
-                                        char *cwd = malloc(150);
-                                        getcwd(cwd, 150);
-                                        printf("%s\n", cwd);
-                                        free(cwd);
+                                if (strncmp(command, "cd", 2) == 0) {
+                                        if (parameters == NULL || strlen(parameters) == 0) {
+                                                char *cwd = malloc(150);
+                                                getcwd(cwd, 150);
+                                                printf("%s\n", cwd);
+                                                free(cwd);
+                                        } else {
+                                                chdir(parameters);
+                                        }
+                                } else if (strncmp(command, "exit", 4) == 0) {
+                                        raise(SIGTERM);
+                                } else if (strncmp(command, "pid", 3) == 0) {
+                                        printf("PID: %d\n", getpid());
+                                } else if (strncmp(command, "ppid", 4) == 0) {
+                                        printf("PPID: %d\n", getppid());
                                 } else {
-                                        chdir(parameters);
-                                }
-                        } else if (strncmp(command, "exit", 4) == 0) {
+                                        // just exec
+                                        pid_t child_pid = fork();
+                                        if(child_pid == 0) {
+                                                char* arg[50];
+                                                arg[0] = strtok(copy, " ");
 
-                        } else if (strncmp(command, "pid", 3) == 0) {
-                                printf("PID: %d\n", getpid());
-                        } else if (strncmp(command, "ppid", 4) == 0) {
-                                printf("PPID: %d\n", getppid());
-                        } else {
-                                // just exec
-                                pid_t child_pid = fork();
-                                if(child_pid == 0) {
-                                        // execvp(command, &parameters);
-                                        // char *const *s = malloc(10);
-                                        // *s = "ls";
-                                        // execvp("ls", s);
+                                                int i = 1;
+                                                do {
 
-                                        char* arg[] = {command, parameters, NULL};
-                                        execvp(arg[0],arg);
+                                                        arg[i] = strtok(NULL, " ");
+                                                        i++;
+                                                }
+                                                while (arg[i - 1] != NULL);
 
-                                        printf("Command not found.\n");
-                                        exit(0);
-                                }
-                                else {
-                                        int child_status;
-                                        pid_t tpid;
-                                        do {
-                                                tpid = wait(&child_status);
-                                        } while(tpid != child_pid);
+                                                execvp(arg[0], arg);
+
+                                                printf("Command not found.\n");
+                                                exit(0);
+                                        }
+                                        else {
+                                                int child_status;
+                                                pid_t tpid;
+                                                do {
+                                                        tpid = wait(&child_status);
+                                                } while(tpid != child_pid);
+                                                printf("Exited with status: %d\n", child_status);
+                                        }
                                 }
                         }
                 }
